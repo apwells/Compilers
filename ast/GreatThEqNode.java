a14332f6fcdff2f9e998ba02a4780317a58c6315
Alex Wells <thealee@gmail.com>
2013-04-21 20:13:04 +0100

    GreatTheEqNode, GreatTheNode

---
+++ ast/GreatThEqNode.java
@@ -0,0 +1,9 @@
+package ast;
+
+public class GreatThEqNode extends ExprTermNode {
+
+	public GreatTheNode (ExprNode value, TermNode term) {
+		super(value, term);
+	}
+	
+}
---
+++ ast/GreatThNode.java
@@ -0,0 +1,9 @@
+package ast;
+
+public class GreatThNode extends ExprTermNode {
+
+	public GreatTheNode (ExprNode value, TermNode term) {
+		super(value, term);
+	}
+	
+}
