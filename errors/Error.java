ea0ddd669282d48dc38b3b7b3a5f849832d0e1bb
Panos Stylianou <panosstylianou@hotmail.com>
2013-04-22 23:07:45 +0100

    Error

--- errors/Error.java
+++ errors/Error.java
@@ -2,22 +2,39 @@ package errors;
 
 public class Error {
 	
-	
-	private int systaxErrors = 0;
-	public Error(int syntaxErrors) {
-		this.systaxErrors = syntaxErrors;
+	public enum Type{
+		DECL, NOTDECL, TYPE
 	}
 	
-	public static void PrintError(String error){
-		System.out.println("ERROR : " + error);
+	public static void PrintError(String error, Type t){
+		switch (t) {
+		case DECL:
+			System.out.println("Variable " + error + " has already been declared");
+			break;
+		case NOTDECL:
+			System.out.println("Variable " + error + " has not been declared");
+			break;
+		case TYPE:
+			System.out.println("Incorrect type" + error);
+			break;
+		}
+
+ 
+		
 	}
 	
 	/*
 	 * 	Scope Errors
 	 */
+<<<<<<< HEAD
 
 	public static void VariableAlreadyDeclared(String id){
 		PrintError("Variable " +id+" Already Declared");
+=======
+/*
+	public static void VariableAlreadyDeclared(){
+		PrintError("Variable Already Declared");
+>>>>>>> Error
 	}
-	
+	*/
 }
