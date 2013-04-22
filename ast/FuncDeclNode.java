1bc860dae48cdc461f87f1c8e472e824db6d1d45
Alex Wells <thealee@gmail.com>
2013-04-22 01:35:19 +0100

    Changed FuncDeclNode to include block

--- ast/FuncDeclNode.java
+++ ast/FuncDeclNode.java
@@ -5,11 +5,13 @@ public class FuncDeclNode extends DeclNode {
 	protected ParamListNode params;
 	protected String id;
 	protected TypeNode type;
+	protected BlockNode block;
 	
-	public FuncDeclNode (ParamListNode params, String id, TypeNode type) {
+	public FuncDeclNode (ParamListNode params, String id, TypeNode type, BlockNode block) {
 		this.params = params;
 		this.type = type;
 		this.id = id;
+		this.block = block;
 	}
 	public Object accept(Visitor v) {
 		return v.visit(this);
--- symboltable/SymbolTable.java
+++ symboltable/SymbolTable.java
@@ -7,18 +7,22 @@ public class SymbolTable {
 	
 	private SymbolTable parent;
 	private ArrayList<SymbolTable> children;
+	private Hashtable<String, Object> table;
 	
 	public SymbolTable() {
 		// Initialize
-		
+		table = new Hashtable<String, Object>();
 	}
 	
 	public SymbolTable put (String key, Object value) {
 		// Insert a new binding
+		table.put(key, value);
+		return this;
 	}
 	
 	public Object get (String key) {
 		// look up a binding
+		return table.get(key);
 	}
 	
 	public SymbolTable beginScope() {
