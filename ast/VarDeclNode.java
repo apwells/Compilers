f0dfd7ae2383fdec31c47a8b0b41b124ec638a4e
Eduard Mucilianu <eduard.mucilianu@yahoo.com>
2013-04-23 18:04:09 +0100

    hahaha

--- ast/VarDeclNode.java
+++ ast/VarDeclNode.java
@@ -6,7 +6,8 @@ public class VarDeclNode extends DeclNode{
 	protected VarTypeNode id;
 	
 	public VarDeclNode (ExprListNode declarations, VarTypeNode id) {
-		this.declarations = declarations;
+		this.declarations = new ExprListNode(null);
+        if(declarations!=null) this.declarations = declarations;
 		this.id = id;
 	}
 	public Object accept(Visitor v) {
