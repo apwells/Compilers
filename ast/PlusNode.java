23e9b3f78af5566fe22effb17af45630054c6135
Alex Wells <thealee@gmail.com>
2013-04-24 04:15:40 +0100

    adf

--- ast/PlusNode.java
+++ ast/PlusNode.java
@@ -4,7 +4,6 @@ public class PlusNode extends ExprTermNode {
 
 	public PlusNode(ExprNode value, TermNode term) {
 		super(value, term);
-		System.out.println("Value = " + value + ", Term = " + term);
 	}
 	public Object accept(Visitor v) {
 		return v.visit(this);
